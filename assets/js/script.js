2f9c3ec8b52846b02e47ffdd1d4b69332c40fc43
PRAVEEN200307 <crouselearn@gmail.com>
2023-12-10 08:36:42 +0530

    update side navbar;

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,6 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    const sideNav=document.querySelector('.side-nav');
-    const btnNav=document.querySelector('#btnNav');
-    const OverlayScreen=document.querySelector('.navoverlay');
-
-    btnNav.addEventListener('click',()=>{
-        sideNav.classList.add('opens');
-    });
-
-    OverlayScreen.addEventListener('click',()=>{
-        sideNav.classList.remove('opens');
-    });
-
+const point =document.getElementsByClassName('point')[0];
+const btnNav=document.querySelector('#btnNav');
     
-   
-});
+    btnNav.addEventListener('click',function(){
+        point.classList.toggle('side_nevbar')   
+    });
